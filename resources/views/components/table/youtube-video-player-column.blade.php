0c68dbbdff3dccb1e430f7993fd981912f5c73ef
soctrap <60925791+soctrap@users.noreply.github.com>
2024-03-12 14:24:49 +0300

    Update youtube-video-player-column.blade.php

--- resources/views/components/table/youtube-video-player-column.blade.php
+++ resources/views/components/table/youtube-video-player-column.blade.php
@@ -1,12 +1,17 @@
-<div style="{{ $getCssAsString() }}"
-    data-plyr-config='{
-        {{ $getConfigAsString() }}
-    }'
-    class="plyr__video-embed js-plyr">
-    <iframe 
-        style="position: unset;"
-        src="{{ $getState() }}"
-        allowfullscreen
-        allowtransparency
-        allow="autoplay"></iframe>
+<div
+    x-data="plyr"
+    wire:ignore
+>
+    <div style="{{ $getCssAsString() }}"
+        data-plyr-config='{
+            {{ $getConfigAsString() }}
+        }'
+        class="plyr__video-embed js-plyr">
+        <iframe 
+            style="position: unset;"
+            src="{{ $getState() }}"
+            allowfullscreen
+            allowtransparency
+            allow="autoplay"></iframe>
+    </div>
 </div>
