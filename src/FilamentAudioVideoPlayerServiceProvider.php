56eba89a5a259ca9cb927f461bbf9e04cf68dbaa
soctrap <60925791+soctrap@users.noreply.github.com>
2024-03-12 14:22:40 +0300

    Update FilamentAudioVideoPlayerServiceProvider.php

--- src/FilamentAudioVideoPlayerServiceProvider.php
+++ src/FilamentAudioVideoPlayerServiceProvider.php
@@ -2,12 +2,11 @@
 
 namespace AymanAlhattami\FilamentAudioVideoPlayer;
 
-use Filament\Facades\Filament;
-use Illuminate\Support\HtmlString;
-use Filament\PluginServiceProvider;
+use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
+use Spatie\LaravelPackageTools\PackageServiceProvider;
 
-class FilamentAudioVideoPlayerServiceProvider extends PluginServiceProvider
+class FilamentAudioVideoPlayerServiceProvider extends PackageServiceProvider
 {
     public static string $name = 'filament-audio-video-player';
 
@@ -16,27 +15,5 @@ class FilamentAudioVideoPlayerServiceProvider extends PluginServiceProvider
         $package->name(static::$name)
             ->hasViews()
             ->hasConfigFile();
-    }
-
-    public function packageBooted(): void
-    {
-        parent::packageBooted();
-
-        Filament::registerRenderHook(
-            name: 'styles.end',
-            callback: fn () => new HtmlString(html: "
-                <link rel='stylesheet' href='https://cdn.plyr.io/3.7.8/plyr.css' />
-            ")
-        );
-
-        Filament::registerRenderHook(
-            name: 'scripts.end',
-            callback: fn () => new HtmlString(html: "
-                <script src='https://cdn.plyr.io/3.7.8/plyr.polyfilled.js'></script>
-                <script>
-                    Array.from(document.querySelectorAll('.js-plyr')).map((p) => new Plyr(p));
-                </script>
-            ")
-        );
     }
 }
