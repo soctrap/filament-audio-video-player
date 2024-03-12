e59e5361090d1aececaa45bfdc7cda37156aa5b2
soctrap <60925791+soctrap@users.noreply.github.com>
2024-03-12 14:24:12 +0300

    Update video-player-column.blade.php

--- resources/views/components/table/video-player-column.blade.php
+++ resources/views/components/table/video-player-column.blade.php
@@ -1,10 +1,15 @@
-<video style="{{ $getCssAsString() }}"
-    data-plyr-config='{
-        {{ $getConfigAsString() }}
-    }'
-    class="js-plyr"
-    playsinline
-    controls>
-    <source src="{{ $getState() }}"
-        type="video/mp4">
-</video>
+<div
+    x-data="plyr"
+    wire:ignore
+>
+    <video style="{{ $getCssAsString() }}"
+        data-plyr-config='{
+            {{ $getConfigAsString() }}
+        }'
+        class="js-plyr"
+        playsinline
+        controls>
+        <source src="{{ $getState() }}"
+            type="video/mp4">
+    </video>
+</div>
