9edb76e64ac9c68e30067a9ae55f0c9906bda8a5
soctrap <60925791+soctrap@users.noreply.github.com>
2024-03-12 14:23:50 +0300

    Update audio-player-column.blade.php

--- resources/views/components/table/audio-player-column.blade.php
+++ resources/views/components/table/audio-player-column.blade.php
@@ -1,9 +1,14 @@
-<audio style="{{ $getCssAsString() }}"
-    data-plyr-config='{
-        {{ $getConfigAsString() }}
-    }'
-    class="js-plyr"
-    controls>
-    <source src="{{ $getState() }}"
-        type="audio/mp3" />
-</audio>
+<div
+    x-data="plyr"
+    wire:ignore
+>
+    <audio style="{{ $getCssAsString() }}"
+        data-plyr-config='{
+            {{ $getConfigAsString() }}
+        }'
+        class="js-plyr"
+        controls>
+        <source src="{{ $getState() }}"
+            type="audio/mp3" />
+    </audio>
+</div>
